ef1cc8624bdb0c44f578b50c70e49fe62d3ca303
colinrieger <rieger1085@gmail.com>
2018-09-04 14:26:10 -0600

    Add main menu game buttons after sizing is done.

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -26,12 +26,6 @@ public class MainMenu : MonoBehaviour
         m_GameScrollViewContent = transform.Find("GameScrollView/Viewport/Content").gameObject;
     }
 
-    private void Start()
-    {
-        foreach (GameData gameData in m_GameData)
-            AddGameButton(gameData);
-    }
-
     void Update()
     {
         if (!m_GameScrollViewSized)
@@ -40,18 +34,22 @@ public class MainMenu : MonoBehaviour
 
     void SizeGameScrollView()
     {
+        // the scroll view takes a few frames to size
         if (m_GameScrollViewContent.GetComponent<RectTransform>().rect.width == 0)
             return;
 
         RectTransform rectTransform = m_GameScrollViewContent.GetComponent<RectTransform>();
         GridLayoutGroup gridLayoutGroup = m_GameScrollViewContent.GetComponent<GridLayoutGroup>();
         int columns = gridLayoutGroup.constraintCount;
-        int rows = (m_GameScrollViewContent.transform.childCount + columns - 1) / columns;
+        int rows = (m_GameData.Count + columns - 1) / columns;
         float viewWidth = rectTransform.rect.width - (gridLayoutGroup.spacing.x * (columns - 1));
         float buttonWidth = viewWidth / columns;
         float viewHeight = (buttonWidth * rows) + (gridLayoutGroup.spacing.y * (rows - 1));
         m_GameScrollViewContent.GetComponent<GridLayoutGroup>().cellSize = new Vector2(buttonWidth, buttonWidth);
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, viewHeight);
+
+        foreach (GameData gameData in m_GameData)
+            AddGameButton(gameData);
 
         m_GameScrollViewSized = true;
     }
