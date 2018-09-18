3a58bee15da0bb62b1ae804470d9526537e68def
colinrieger <rieger1085@gmail.com>
2018-09-18 17:17:12 -0600

    Hooked up meatball eating and tail growth.

---
+++ Assets/Scripts/Noodle/Meatball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Noodle
+{
+    public class Meatball : MonoBehaviour
+    {
+        public Transform TopWall;
+        public Transform BottomWall;
+        public Transform LeftWall;
+        public Transform RightWall;
+        
+        private List<Vector3> m_Positions = new List<Vector3>();
+
+        void Start()
+        {
+            Random.InitState(System.DateTime.Now.Millisecond);
+
+            int minX = (int)(LeftWall.localPosition.x + (LeftWall.localScale.x / 2.0f));
+            int maxX = (int)(RightWall.localPosition.x - (RightWall.localScale.x / 2.0f) - transform.localScale.x);
+            int minY = (int)(BottomWall.localPosition.y + (BottomWall.localScale.y / 2.0f));
+            int maxY = (int)(TopWall.localPosition.y - (BottomWall.localScale.y / 2.0f) - transform.localScale.y);
+
+            for (int x = minX; x <= maxX; x += (int)transform.localScale.x)
+                for (int y = minY; y <= maxY; y += (int)transform.localScale.y)
+                    m_Positions.Add(new Vector3(x, y));
+        }
+
+        private void RandomlyPlace()
+        {
+            Vector3 position;
+            do
+                position = m_Positions[Random.Range(0, m_Positions.Count)];
+            while (position == transform.localPosition);
+            // don't put it back in the same spot
+            // since the noodle has the same position (due to the collision), this doubles for noodle position check
+            // if we place the meatball on a noodle tail, it will enter the trigger code (without growing) and attempt to place again
+
+            gameObject.transform.localPosition = position;
+            gameObject.SetActive(true);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            gameObject.SetActive(false);
+
+            RandomlyPlace();
+        }
+    }
+}
--- Assets/Scripts/Noodle/Noodle.cs
+++ Assets/Scripts/Noodle/Noodle.cs
@@ -6,9 +6,12 @@ namespace Noodle
 {
     public class Noodle : MonoBehaviour
     {
-        private Vector3 m_Direction = Vector3.zero;
+        public GameObject TailPrefab;
 
+        private Vector3 m_Direction = Vector3.zero;
         private Queue<Vector3> m_InputQueue = new Queue<Vector3>();
+        private Queue<Transform> m_TailQueue = new Queue<Transform>();
+        private bool m_Grow = false;
 
         private const float m_TimeStep = .2f;
 
@@ -30,15 +33,15 @@ namespace Noodle
 
         private void AddInput(Vector3 input)
         {
-            Vector3 inputCompare = (m_InputQueue.Count > 0) ? m_InputQueue.Last() : m_Direction;
+            Vector3 lastInput = (m_InputQueue.Count > 0) ? m_InputQueue.Last() : m_Direction;
             
-            if (inputCompare == input)
+            if (lastInput == input)
                 return;
 
-            if (inputCompare.x != 0f && input.x != 0f)
+            if (lastInput.x != 0f && input.x != 0f)
                 return;
 
-            if (inputCompare.y != 0f && input.y != 0f)
+            if (lastInput.y != 0f && input.y != 0f)
                 return;
 
             m_InputQueue.Enqueue(input);
@@ -46,6 +49,11 @@ namespace Noodle
 
         private void ResetNoodle()
         {
+            foreach (Transform tail in m_TailQueue.ToList())
+            {
+                GameObject.Destroy(tail.gameObject);
+            }
+            m_TailQueue.Clear();
             m_InputQueue.Clear();
             m_Direction = Vector3.zero;
             transform.position = Vector3.zero;
@@ -53,10 +61,26 @@ namespace Noodle
 
         private void Move()
         {
+
             if (m_InputQueue.Count != 0)
                 m_Direction = m_InputQueue.Dequeue();
             
+            Vector3 lastPosition = transform.position;
+
             transform.localPosition += (transform.localScale.x * m_Direction);
+            if (m_Grow)
+            {
+                GameObject newTail = Instantiate(TailPrefab, lastPosition, new Quaternion(), transform.parent) as GameObject;
+                newTail.name = TailPrefab.name; // remove (Clone), for trigger code
+                m_TailQueue.Enqueue(newTail.transform);
+                m_Grow = false;
+            }
+            else if (m_TailQueue.Count > 0)
+            {
+                Transform tail = m_TailQueue.Dequeue();
+                tail.position = lastPosition;
+                m_TailQueue.Enqueue(tail);
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D collider)
@@ -67,9 +91,11 @@ namespace Noodle
                 case "BottomWall":
                 case "LeftWall":
                 case "RightWall":
-                    {
-                        ResetNoodle();
-                    }
+                case "Tail":
+                    ResetNoodle();
+                    break;
+                case "Meatball":
+                    m_Grow = true;
                     break;
                 default:
                     break;
